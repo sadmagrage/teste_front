2c0001f00e378f83acca3ebc8f269f0d67969fcb
sadmagrage <victorgoyapvgp09@gmail.com>
2023-11-07 23:59:05 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Axios from "axios";
 import Cookies from "js-cookie";
+
 import { useState } from "react";
 
 export default function App () {
@@ -8,7 +9,7 @@ export default function App () {
   const [cookieResponse, setCookieResponse] = useState("");
 
   const axios = Axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://ebony-jumpy-spinosaurus.glitch.me",
     withCredentials: true
 
   });
